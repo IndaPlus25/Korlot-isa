b37221ef355567b259fdeb47c8d82fc9d5d44351
FilipK <korlot@kth.se>
2025-11-14 12:55:42 +0100

    updating

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Korlot-isa
+#######################################################
+##Sheet for how to write the commands for .ffes-files##
+#######################################################
+
+Intructions -   4 bit
+Registers   -   3 bit
+Imm         -   9 bit
+------------------------------------------------------------------------------------------------------
+rd - destination register
+rs1 = file source 1
+rs2 = file source 2
+imm = integer x, 0 <= x >= 511
+ra1 = destination for jump1
+ra2 = destination for jump2
+
+------------------------------------------------------------------------------------------------------
+Math:
+    madd rd rs1/imm rs2/imm     |   Add rs1/imm and rs2/imm and puts it in rd
+    
+    msub rd rs1/imm rs2/imm     |   Subtract rs1/imm and rs2/imm and puts it in rd
+------------------------------------------------------------------------------------------------------
+Jump:
+    jjt1                    |   Jump to ra1
+    
+    jjt2                    |   Jump to ra2
+------------------------------------------------------------------------------------------------------
+Logic:
+    lgrt x rs1 imm          |   If rs1 > imm do x
+    
+    llst x rs1 imm          |   If rs1 < imm do x
+    
+    leql x rs1 imm          |   If rs1 = imm do x
+------------------------------------------------------------------------------------------------------
+Special:
+    sprt rs1                |   Print stored element
+    
+    semp rs1                |   Clear stored element
+    
+    scpy rd rs1             |   Copy element from rs1 to rd
+    
+    sbrk                    |   Quit the program
+    
+------------------------------------------------------------------------------------------------------
+
--- interpreter.py
+++ interpreter.py
@@ -1,176 +1,167 @@
-"""
-    This code is of a basic interpreter for the made up language
-    bbvv, which is a simple 8-bit version of assembly
-
-    Author: Tobias Hansson <Tohanss@kth.se>
-    *Updated by Filip Körlof <Korlot@kth.se>
-
-    **Updated to 16-bit version of assembly.**
-    **Instructions are increased. 
-    
-            
-"""
-import pathlib
 import sys
+import pathlib
 import collections
 
-# Our assembly language has 4 registers
-registers = [0, 0, 0, 0]
+#Create starting items
+a0 = []
+a1 = []
+a2 = []
+a3 = []
+a4 = []
+a5 = []
+ra1 = 0
+ra2 = 0
 
-# 3 lists containing the different types of instructions we allow
-r_type = ["add", "sub", "set", "jeq"]
-j_type = ["j"]
-spec_type = ["input", "print", "exit"]
+pointer = 0
 
-"""
-    Parses a file containing code formatted as
-    a single string into instructions and remove comments
+file_pointer_list = []
+prepared_instructions = []
 
-    Args:
-        file: The relative path to a code file
-    Returns:
-        A list of strings where each string is a processor instruction
-"""
-def read_instruction(file: pathlib.Path) -> [str]:
-    # Create a list with one element for each line of code
-    lines = file.read_text().split("\n")
-    instructions = []
-    # For each line, remove comments and trailing whitespace
-    for line in lines:
-        end_index = len(line) if line.find("//") == -1 else line.find("//")
-        instructions.append(line[0:end_index].strip())
-    return instructions
+test = 0
 
+# function M
+def m_function(type):
 
-# Read from stdio into #1, per specification
-def read_input():
-    registers[1] = int(input())
+    global file_pointer_list
 
-
-# Print content of register #1, per specification
-def println():
-    print(f"{registers[1]}\n")
-
-
-"""
-    Reads a r_type instruction and gives usefull values,
-    reduces code duplication
-
-    Args:
-        current_inst: The current instruction as a formatted string
-    Returns:
-        reg1: the integer value representing register #1
-        reg2: the integer value representing register #2
-        imm:: the immidiate value 0 or 1
-"""
-def parse_r_type(current_inst):
-    reg1 = int(current_inst[1].strip("#"))
-    reg2 = int(current_inst[2].strip("#"))
-    imm = int(current_inst[3])
-    return reg1, reg2, imm
-
-
-"""
-    Confirms that the code does not contain any syntax errors.
-
-    Args:
-        instructions: A list of instructions represented
-            as properly formatted strings
-    Returns:
-        1 if instructions are valid, else 0 if valid.
-"""
-def valid_instructions(instructions: [str]):
-    error = 0
-    for index, line in enumerate(instructions, 1):
-        parts = line.split(" ")
-        if parts[0] in r_type:
-            if len(parts) != 4:
-                print(
-                    f"\tError on line {index}: Incorrect instruction of length {len(parts)}"
-                )
-                error = 1
-            else:
-                for x in [1, 2]:
-                    if parts[x][0] not in "#":
-                        print(
-                            f"\tError on line {index}: Missing '#' to mark register register"
-                        )
-                        error = 1
-                if parts[1][1] not in ["1", "2", "3"]:
-                    print(
-                        f"\tError on line {index}: {parts[1][1]} cannot be used as first register argument for r_type instructions"
-                    )
-                    error = 1
-                if parts[3] not in ["0", "1"]:
-                    print(
-                        f"\tError on line {index}: invalid immidiate. Can only be a 1 bit value"
-                    )
-                    error = 1
-        elif parts[0] in j_type:
-            if len(parts) != 2:
-                print(
-                    f"\tError on line {index}: Incorrect instruction of length {len(parts)}"
-                )
-                error = 1
-            if int(parts[1]) > 15 or int(parts[1]) < -16:
-                print(
-                    f"\tError on line {index}: {parts[1]} does not fit in a 5 bit (signed) value"
-                )
-                error = 1
-        elif parts[0] in spec_type:
-            if len(parts) != 1:
-                print(
-                    f"\tError on line {index}: Incorrect instruction of length {len(parts)}"
-                )
-                error = 1
+    # Addition with reg/imm and reg/imm
+    if type == "madd":
+        if type(file_pointer_list[2]) == list:
+            val_1 = file_pointer_list[2][0]
         else:
-            if parts[0] != "":
-                print(f"\tError on line {index}: unknown command: {parts[0]}")
-                error = 1
-    return error
+            val_1 = file_pointer_list[2]
+        if type(file_pointer_list[3]) == list:
+            val_1 = file_pointer_list[3][0]
+        else:
+            val_1 = file_pointer_list[3]
+        val_1 = file_pointer_list[2]
+        val_2 = file_pointer_list[3]
+        file_pointer_list[1].clear()
+        file_pointer_list[1].append(val_1 + val_2)
+
+    #Subtraction with reg and reg/imm
+    elif type == "msub":
+        if type(file_pointer_list[2]) == list:
+            val_1 = file_pointer_list[2][0]
+        else:
+            val_1 = file_pointer_list[2]
+        if type(file_pointer_list[3]) == list:
+            val_1 = file_pointer_list[3][0]
+        else:
+            val_1 = file_pointer_list[3]
+        val_1 = file_pointer_list[2]
+        val_2 = file_pointer_list[3]
+        file_pointer_list[1].clear()
+        file_pointer_list[1].append(val_1 - val_2)
+
+    reader()
+
+# function J
+def j_function(type):
+
+    global pointer
+
+    #CHoose ra and jump to index
+    if type == "jjt1":
+        pointer = ra1
+    
+    elif type == "jjt2":
+        pointer = ra2
+
+    reader()
+
+# function s
+def s_function(type):
+
+    global file_pointer_list
+
+    #Print
+    if type == "sprt":
+        my_print = file_pointer_list[1][0].str()
+        print(my_print)
+
+    #Exit file
+    elif type == "sbrk":
+        quit()
+
+    #Empty register
+    elif type == "semp":
+        file_pointer_list[1].clear
+
+    #Copy register
+    elif type == "scpy":
+        file_pointer_list[1] = file_pointer_list[2]
+
+    reader()
+
+# function L
+def l_function(type):
+
+    global file_pointer_list
+
+    #Greater then
+    if type == "lgrt":
+        val_1 = file_pointer_list[2][0]
+        val_2 = file_pointer_list[3][0]
+        if val_1 > val_2:
+            file_pointer_list[1]()
+
+    #Less then
+    elif type == "llst":
+        val_1 = file_pointer_list[2][0]
+        val_2 = file_pointer_list[3][0]
+        if val_1 < val_2:
+            file_pointer_list[1]()
+    #Equal
+    elif type == "leql":
+        val_1 = file_pointer_list[2][0]
+        val_2 = file_pointer_list[3][0]
+        if val_1 == val_2:
+            file_pointer_list[1]()
+
+    reader()
+
+#Execute the fileinstructions
+def reader():
+
+    global test 
+    global pointer
+    global file_pointer_list
+    global prepared_instructions
+
+    print ("reads pointer", pointer)
+    test += 1
+    print (test, " times reader run")
+
+    file_pointer_list = prepared_instructions[pointer]
+    decide_function = file_pointer_list.split()
+    pointer += 1
+    print(type(decide_function))
+    match decide_function[0]:
+
+        case "madd" | "msub":
+            m_function(decide_function)
+
+        case "jjt1" | "jjt2":
+            j_function(decide_function[0])
+        
+        case "sprt" | "sbrk" | "semp" | "scpy":
+            s_function(decide_function[0])
+
+        case "lgrt" | "llst" | "leql":
+            s_function(decide_function[0])
 
 
-"""
-    The acutal parsing algorithm. Does something for each line of code.
 
-    Args:
-        instructions: List of properly formatted cpu instructions
-"""
-def parse_instructions(instructions: [str]):
-    index = 0
-    while index < len(instructions):
-        current_inst = instructions[index].split(" ")
-        instruction = current_inst[0]
-        if instruction == "input":
-            read_input()
-        elif instruction == "exit":
-            break
-        elif instruction == "print":
-            println()
-        elif instruction == "add":
-            reg1, reg2, imm = parse_r_type(current_inst)
-            registers[reg1] = registers[reg1] + registers[reg2] + imm
-        elif instruction == "sub":
-            reg1, reg2, imm = parse_r_type(current_inst)
-            registers[reg1] = registers[reg1] - registers[reg2] - imm
-        elif instruction == "set":
-            reg1, reg2, imm = parse_r_type(current_inst)
-            registers[reg1] = registers[reg2] + imm
-        elif instruction == "j":
-            index += int(current_inst[1])
-        elif instruction == "jeq":
-            reg1, reg2, imm = parse_r_type(current_inst)
-            # jeq increments the PC by imm if the compared registers are equal
-            # otherwise it increment the the PC with ~PC, i.e it does the opposite.
-            if registers[reg1] == registers[reg2]:
-                index += imm
-            else:
-                index += 1 if imm == 0 else 0
-        # Always increment PC after each instruction (even if a jump is made)
-        index += 1
-
-
+#Parts taken from bbvv
 def main():
+
+    global pointer
+    global ra1
+    global ra2
+    
+    counter = 0
+
     # Check that the proper amount of Command Line Arguments (CLA) are given
     if len(sys.argv) != 2:
         print(
@@ -181,13 +172,28 @@ def main():
         # Make sure the the given file path is a file that exists
         if file.is_file():
             # Make sure the file has the proper prefix.
-            if file.suffix != ".bbvv":
-                print(f"\tGiven file is not of type .bbvv")
+            if file.suffix != ".ffes":
+                print(f"\tGiven file is not of type .ffes")
             else:
-                instructions = read_instruction(file)
-                if not valid_instructions(instructions):
-                    parse_instructions(instructions)
-        else:
+                full_instructions = open(sys.argv[1], "r")
+                row_split_instruction = full_instructions.readlines()
+                for row in row_split_instruction:
+                        temp_row = row_split_instruction[counter].rstrip()
+                        if temp_row == "ra1":
+                            ra1 = pointer
+                            pointer += 1
+                        elif temp_row == "ra2":
+                            ra2 = pointer
+                            pointer += 1
+                        elif temp_row != "":
+                            prepared_instructions.append(temp_row)
+                            pointer += 1
+                        counter += 1
+                full_instructions.close()
+                print (prepared_instructions[0])
+                pointer = 0
+                reader()
+        else:   
             print(f"\tFile: {file} does not exist")
 
 
